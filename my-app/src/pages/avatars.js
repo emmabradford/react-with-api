a71c3a920f00ac9853e2e86f130bdf877a6b7a1a
emma <emma.abe.bradford@gmail.com>
2022-11-27 17:50:59 -0500

    added curr

--- my-app/src/pages/avatars.js
+++ my-app/src/pages/avatars.js
@@ -1,44 +1,63 @@
-
 import React, { useEffect, useState } from "react";
 import axios from 'axios';
 
 
-
 function Avatars() {
+
+
     const [Data, setData] = useState({
         name: '',
         photoUrl: '',
         affiliation: '',
         allies: [],
         enemies: [],
-        position: '',
-        predecessor: ''
+        position: ''
     })
     useEffect(() => {
         axios.get('https://last-airbender-api.herokuapp.com/api/v1/characters/avatar')
             .then(res => {
                 console.log('Response from main API: ', res)
-                console.log('avatarData : ', res.data[0])
-                setData(res.data)
+                console.log('avatar Data: ', res.data[0])
+                res.data.forEach(element=>{
+                    console.log(element);
+                    // let randomCharData = element;
 
-                // let randomCharData = res.data[0];
-                // setData({ name: randomCharData.name, photoUrl: randomCharData.photoUrl, affiliation: randomCharData.affiliation, allies: randomCharData.allies,  enemies: randomCharData.enemies, position: randomCharData.position})
+                    // setData({ name: randomCharData.name, photoUrl: randomCharData.photoUrl, affiliation: randomCharData.affiliation, allies: randomCharData.allies, enemies: randomCharData.enemies, position: randomCharData.position })
+                    setData(res.data);
+                }
+
+                )
+                // ata({ name: randomCharData.name, photoUrl: randomCharData.photoUrl, affiliation: randomCharData.affiliation, allies: randomCharData.allies, enemies: randomCharData.enemies, position: randomCharData.position })
             })
             .catch(err => {
-                console.log('not working');
-                //console.log(err);
+                Data = [{name: '',
+                photoUrl: '',
+                affiliation: '',
+                allies: [],
+                enemies: [],
+                position: ''}];
+
+                console.log("hi");
+
+                console.log(err);
             })
     }, [])
-    // console.log("Data");
-    let index = 2;
+    let index = 0;
+    let curr = Data[0];
+
+    console.log("curr");
+
+     console.log(curr);
     function addIndex() {
-        if (index < Data.length-1) {
+        if (index < Data.length - 1) {
             index = index + 1;
         }
         else {
             index = 0;
         }
         console.log(index);
+        curr = Data[index];
+        console.log(curr);
     }
 
     function subIndex() {
@@ -46,28 +65,30 @@ function Avatars() {
             index = index - 1;
         }
         else {
-            index = Data.length-1;
+            index = Data.length - 1;
         }
         console.log(index);
+        curr = Data[index];
+        console.log(curr);
     }
 
-    return (
+   return (
 
         <div>
             <button onClick={subIndex}>Prev</button>
 
             <button onClick={addIndex}>Next</button>
-            {/* <h1> Name: {Data[index].name}</h1>
-            <h1> affiliation: {Data[index].affiliation}</h1>
-            <h1> allies: {Data[index].allies}</h1>
-            <h1> enemies: {Data[index].enemies}</h1>
-            <h1> position: {Data[index].position}</h1>
-            <h1> predecessor: {Data[index].predecessor}</h1>
+            {/* <h1> Name: {Data[0].name}</h1>
+            <h1> affiliation: {Data[0].affiliation}</h1>
+            <h1> allies: {Data[0].allies}</h1>
+            <h1> enemies: {Data[0].enemies}</h1>
+            <h1> position: {Data[0].position}</h1>
 
-            <img src={Data[index].photoUrl} alt={Data[index].name} /> */}
+            <img src={Data[0].photoUrl} alt={Data[0].name} /> */}
 
         </div>
     );
+
 };
 
 export default Avatars;
