23ffc80757589b30503a309309a82fb5bb770729
emma <emma.abe.bradford@gmail.com>
2022-11-27 14:29:23 -0500

    changed stying on nav bar

--- my-app/src/components/navbar/index.js
+++ my-app/src/components/navbar/index.js
@@ -1,28 +1,38 @@
 import React from "react";
-import { Nav, NavLink, NavMenu } 
+import { Nav, NavLink, NavMenu }
     from "./navBarElements";
-  
+
 const Navbar = () => {
-  return (
-    <>
-      <Nav>
-        <NavMenu>
-        <NavLink to="/" activeStyle>
-            Home
-          </NavLink>
-          <NavLink to="/avatars" activeStyle>
-            Avatars
-          </NavLink>
-          <NavLink to="/bending" activeStyle>
-            Bending
-          </NavLink>
-          <NavLink to="/Locations" activeStyle>
-            Locations
-          </NavLink>
-        </NavMenu>
-      </Nav>
-    </>
-  );
+    return (
+ 
+        
+
+        <div className="NavBar">
+            <nav class="navbar navbar-default">
+                <div class="container-fluid">
+                    <div class="navbar-header">
+                        <a class="navbar-brand" href="#">ATLA API</a>
+                    </div>
+                    <ul class="nav navbar-nav">
+
+                    <NavLink to="/" activeStyle>
+                        Random Character
+                    </NavLink>
+                    <NavLink to="/avatars" activeStyle>
+                        Avatars
+                    </NavLink>
+                    <NavLink to="/bending" activeStyle>
+                        Bending
+                    </NavLink>
+                    <NavLink to="/Locations" activeStyle>
+                        Locations
+                    </NavLink>
+                    </ul>
+                </div>
+            </nav >
+        </div >
+
+    );
 };
-  
+
 export default Navbar;
