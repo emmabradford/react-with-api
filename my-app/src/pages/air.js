0018b269f9a7ffa38bf9c6ba62d1b38b0f494f31
emma <emma.abe.bradford@gmail.com>
2022-11-27 19:17:39 -0500

    fixed up airbending

--- my-app/src/pages/air.js
+++ my-app/src/pages/air.js
@@ -3,19 +3,32 @@ import axios from 'axios';
 const Air = () => {
   const [Data, setData] = useState({
     name: '',
-    photoUrl: '',
-    affiliation: '',
-    allies: [],
-    enemies: [],
-    position: ''
+    inspiration: '',
+    pioneers: [],
+    notableUsers: [],
+    strengths: [],
+    weaknesses: [],
+    techniques:[],
+    advancedTechniques: [],
+    description:''
+
   })
   useEffect(() => {
-    axios.get('https://last-airbender-api.herokuapp.com/api/v1/characters/random')
+    axios.get('https://avatar-the-last-airbender-api.herokuapp.com/api/v1/bending/airbending')
       .then(res => {
         console.log('Response from main API: ', res)
-        console.log('Home Data: ', res.data[0])
-        let randomCharData = res.data[0];
-        setData({ name: randomCharData.name, photoUrl: randomCharData.photoUrl, affiliation: randomCharData.affiliation, allies: randomCharData.allies, enemies: randomCharData.enemies, position: randomCharData.position })
+        console.log('air Data: ', res.data)
+        let airData = res.data;
+        setData({ name: airData.name, 
+          pioneers: airData.pioneers, 
+          inspiration: airData.inspiration, 
+          notableUsers: airData.notableUsers, 
+          strengths: airData.strengths, 
+          weaknesses: airData.weaknesses,
+          techniques: airData.techniques,
+          advancedTechniques: airData.advancedTechniques,
+          description: airData.description
+         })
       })
       .catch(err => {
         console.log(err);
@@ -25,12 +38,15 @@ const Air = () => {
 
     <div>
       <h1> Name: {Data.name}</h1>
-      <h1> affiliation: {Data.affiliation}</h1>
-      <h1> allies: {Data.allies}</h1>
-      <h1> enemies: {Data.enemies}</h1>
-      <h1> position: {Data.position}</h1>
-
-      <img src={Data.photoUrl} alt={Data.name} />
+      <h1> inspiration: {Data.inspiration}</h1>
+      <h1> pioneers: {Data.pioneers}</h1>
+      <h1> notableUsers: {Data.notableUsers}</h1>
+      <h1> strengths: {Data.strengths}</h1>
+      <h1> weaknesses: {Data.weaknesses}</h1>
+      <h1> techniques: {Data.techniques}</h1>
+      <h1> advancedTechniques: {Data.advancedTechniques}</h1>
+      <h1> description: {Data.description}</h1>
+      
 
     </div>
   );
--- my-app/src/pages/earth.js
+++ my-app/src/pages/earth.js
@@ -3,13 +3,55 @@ import React, { useEffect, useState } from "react";
 import axios from 'axios';  
 
 const Earth = () => {
-  return (
-    <div>
-      <h1>
-      earth
-      </h1>
-    </div>
-  );
+    const [Data, setData] = useState({
+        name: '',
+        inspiration: '',
+        pioneers: [],
+        notableUsers: [],
+        strengths: [],
+        weaknesses: [],
+        techniques:[],
+        advancedTechniques: [],
+        description:''
+    
+      })
+      useEffect(() => {
+        axios.get('https://avatar-the-last-airbender-api.herokuapp.com/api/v1/bending/earthbending')
+          .then(res => {
+            console.log('Response from main API: ', res)
+            console.log('earth Data: ', res.data)
+            let earthData = res.data;
+            setData({ name: earthData.name, 
+              pioneers: earthData.pioneers, 
+              inspiration: earthData.inspiration, 
+              notableUsers: earthData.notableUsers, 
+              strengths: earthData.strengths, 
+              weaknesses: earthData.weaknesses,
+              techniques: earthData.techniques,
+              advancedTechniques: earthData.advancedTechniques,
+              description: earthData.description
+             })
+          })
+          .catch(err => {
+            console.log(err);
+          })
+      }, [])
+      return (
+    
+        <div>
+          <h1> Name: {Data.name}</h1>
+          <h1> inspiration: {Data.inspiration}</h1>
+          <h1> pioneers: {Data.pioneers}</h1>
+          <h1> notableUsers: {Data.notableUsers}</h1>
+          <h1> strengths: {Data.strengths}</h1>
+          <h1> weaknesses: {Data.weaknesses}</h1>
+          <h1> techniques: {Data.techniques}</h1>
+          <h1> advancedTechniques: {Data.advancedTechniques}</h1>
+          <h1> description: {Data.description}</h1>
+          
+    
+        </div>
+      );
 };
   
 export default Earth;
