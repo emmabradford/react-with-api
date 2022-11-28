97c96045098506f970fc0c462e28a2d8f3c81c45
emma <emma.abe.bradford@gmail.com>
2022-11-27 19:19:52 -0500

    fixed up rest of the bending

--- my-app/src/pages/fire.js
+++ my-app/src/pages/fire.js
@@ -1,12 +1,54 @@
 import React, { useEffect, useState } from "react";
 import axios from 'axios';
-  
+
 const Fire = () => {
+  const [Data, setData] = useState({
+    name: '',
+    inspiration: '',
+    pioneers: [],
+    notableUsers: [],
+    strengths: [],
+    weaknesses: [],
+    techniques:[],
+    advancedTechniques: [],
+    description:''
+
+  })
+  useEffect(() => {
+    axios.get('https://avatar-the-last-airbender-api.herokuapp.com/api/v1/bending/firebending')
+      .then(res => {
+        console.log('Response from main API: ', res)
+        console.log('fire Data: ', res.data)
+        let fireData = res.data;
+        setData({ name: fireData.name, 
+          pioneers: fireData.pioneers, 
+          inspiration: fireData.inspiration, 
+          notableUsers: fireData.notableUsers, 
+          strengths: fireData.strengths, 
+          weaknesses: fireData.weaknesses,
+          techniques: fireData.techniques,
+          advancedTechniques: fireData.advancedTechniques,
+          description: fireData.description
+         })
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  }, [])
   return (
+
     <div>
-      <h1>
-      Fire
-      </h1>
+      <h1> Name: {Data.name}</h1>
+      <h1> inspiration: {Data.inspiration}</h1>
+      <h1> pioneers: {Data.pioneers}</h1>
+      <h1> notableUsers: {Data.notableUsers}</h1>
+      <h1> strengths: {Data.strengths}</h1>
+      <h1> weaknesses: {Data.weaknesses}</h1>
+      <h1> techniques: {Data.techniques}</h1>
+      <h1> advancedTechniques: {Data.advancedTechniques}</h1>
+      <h1> description: {Data.description}</h1>
+      
+
     </div>
   );
 };
--- my-app/src/pages/water.js
+++ my-app/src/pages/water.js
@@ -2,11 +2,53 @@ import React, { useEffect, useState } from "react";
 import axios from 'axios';
   
 const Water = () => {
+  const [Data, setData] = useState({
+    name: '',
+    inspiration: '',
+    pioneers: [],
+    notableUsers: [],
+    strengths: [],
+    weaknesses: [],
+    techniques:[],
+    advancedTechniques: [],
+    description:''
+
+  })
+  useEffect(() => {
+    axios.get('https://avatar-the-last-airbender-api.herokuapp.com/api/v1/bending/waterbending')
+      .then(res => {
+        console.log('Response from main API: ', res)
+        console.log('water Data: ', res.data)
+        let waterData = res.data;
+        setData({ name: waterData.name, 
+          pioneers: waterData.pioneers, 
+          inspiration: waterData.inspiration, 
+          notableUsers: waterData.notableUsers, 
+          strengths: waterData.strengths, 
+          weaknesses: waterData.weaknesses,
+          techniques: waterData.techniques,
+          advancedTechniques: waterData.advancedTechniques,
+          description: waterData.description
+         })
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  }, [])
   return (
+
     <div>
-      <h1>
-      Water
-      </h1>
+      <h1> Name: {Data.name}</h1>
+      <h1> inspiration: {Data.inspiration}</h1>
+      <h1> pioneers: {Data.pioneers}</h1>
+      <h1> notableUsers: {Data.notableUsers}</h1>
+      <h1> strengths: {Data.strengths}</h1>
+      <h1> weaknesses: {Data.weaknesses}</h1>
+      <h1> techniques: {Data.techniques}</h1>
+      <h1> advancedTechniques: {Data.advancedTechniques}</h1>
+      <h1> description: {Data.description}</h1>
+      
+
     </div>
   );
 };
